bae074ddd9a5888b03bfc8abed28250660d2c1fa
ajurisic97 <andelo.jurisic@gmail.com>
2022-08-31 14:07:40 +0200

    Added new json files (event and recurring) Added eventRepository for autofill event and recurring

---
+++ CalendarWebAPI/Repositories/EventRepository.cs
@@ -0,0 +1,35 @@
+﻿using CalendarWebAPI.DbModels;
+using System.Text.Json;
+
+namespace CalendarWebAPI.Repositories
+{
+    public class EventRepository
+    {
+        private readonly CalendarContext _dbContext;
+        public EventRepository(CalendarContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public void AddRecurrings()
+        {
+            List<Models.Recurring> listRecurrings = new List<Models.Recurring>();
+            using(StreamReader r = new StreamReader("Recurring.json"))
+            {
+                var recurrings = r.ReadToEnd();
+                listRecurrings = JsonSerializer.Deserialize<List<Models.Recurring>>(recurrings);
+            }
+            
+        }
+        public void AddEvents()
+        {
+            
+        }
+
+        public void AddPerson()
+        {
+            //adding Scheduler after adding Person (And for that we first need Recurring and then Event)
+        }
+    }
+}
